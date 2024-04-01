b2474c8eb0f9426878b824245c9a371c599e02c3
BogdanBargaoanu <bn19bba@yahoo.com>
2024-04-01 23:56:14 +0300

    centered the layout

--- cashflow-client/src/components/CashFlowLog.vue
+++ cashflow-client/src/components/CashFlowLog.vue
@@ -1,17 +1,18 @@
 <template>
     <div id="cashflow-log">
         <div class="accordion" id="accordionExample">
-            <div v-for="log in cashflowLog" :key="log.idcashflowLog" class="accordion-item">
+            <div v-for="log in cashflowLog" :key="log.idcashflowLog" class="accordion-item cashflow-element">
                 <h2 class="accordion-header" id="headingOne">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                         :data-bs-target="'#collapse' + log.idcashflowLog" aria-expanded="false" :aria-controls="'collapse' + log.idcashflowLog">
-                        Collapsible Group Item #1
+                        Transaction ID: {{ log.idcashflowLog }} &nbsp; <b :class="{ 'text-danger': log.value < 0, 'text-success': log.value >= 0 }">{{ log.type }}</b> &nbsp; 
+                        Name: {{ log.name }} &nbsp; Value: {{ log.value }} &nbsp; Currency: {{ log.currency }} &nbsp; Date: {{ log.date }}
                     </button>
                 </h2>
                 <div :id="'collapse' + log.idcashflowLog" class="accordion-collapse collapse" :aria-labelledby="'heading' + log.idcashflowLog"
                     data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                        <select class="name-select" v-model="selectedName">
+                        <select class="name-select" v-model="selectedName" >
                             <option v-for="entity in entities" :key="entity.idEntities">{{ entity.name }}</option>
                         </select>
                     </div>
@@ -19,20 +20,6 @@
             </div>
 
         </div>
-        <ul class="cashflow-list">
-
-            <li v-for="(log, index) in cashflowLog" :key="index">
-                <div class="cashflow-row">
-                    <div class="cashflow-log-id">{{ log.idcashflowLog }}</div>
-                    <div class="cashflow-log-name">{{ log.name }}</div>
-                    <div class="cashflow-log-type">{{ log.type }}</div>
-                    <div class="cashflow-log-amount">{{ log.value }}</div>
-                    <div class="cashflow-log-currency">{{ log.currency }}</div>
-                    <div class="cashflow-log-description">{{ log.description }}</div>
-                    <div class="cashflow-log-date">{{ log.date }}</div>
-                </div>
-            </li>
-        </ul>
     </div>
 </template>
 
@@ -72,10 +59,7 @@ export default {
 </script>
 
 <style scoped>
-#cashflow-log {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 10px;
+#cashflow-log cashflow-element{
     width: 100%;
 }
 </style>
