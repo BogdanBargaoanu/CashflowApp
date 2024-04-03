c43aea99d76c44d9343229cc16ea4d9bf1732235
BogdanBargaoanu <bn19bba@yahoo.com>
2024-04-03 19:26:29 +0300

    modal for insert button

--- cashflow-client/src/components/CashFlowLog.vue
+++ cashflow-client/src/components/CashFlowLog.vue
@@ -81,7 +81,26 @@
                 </div>
             </div>
         </div>
-        <button class="btn-insert">Insert</button>
+        <button type="button" class="btn-insert" data-bs-toggle="modal" data-bs-target="#insertModal">Insert</button>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="insertModal" tabindex="-1" aria-labelledby="InsertPopup" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="InsertPopup">Insert log</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <button @click="logout()" class="btn-logout">Logout</button>
 
