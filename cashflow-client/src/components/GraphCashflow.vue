2d5ce0d27b95111b3716abb6eb3c526792c6a678
BogdanBargaoanu <bn19bba@yahoo.com>
2024-05-31 13:00:32 +0300

    implemented graph

--- cashflow-client/src/components/GraphCashflow.vue
+++ cashflow-client/src/components/GraphCashflow.vue
@@ -8,31 +8,27 @@
 </template>
 <script>
 import { GoogleCharts } from 'google-charts';
-//import axios from 'axios';
+import axios from 'axios';
 export default {
     name: 'GraphCashflow',
     data() {
         return {
             accountValues: { ron: 0, eur: 0, usd: 0 },
             cashflow: [],
+            graphData: [['Date', 'RON', 'EUR', 'USD']],
         }
     },
     methods: {
         drawChart() {
             // Load the Visualization API and the corechart package
             GoogleCharts.load(() => {
+                //console.log(this.graphData);
                 // Create the data table
-                var data = GoogleCharts.api.visualization.arrayToDataTable([
-                    ['Date', 'RON', 'EUR', 'USD'],
-                    ['2004', 1000, 400, 200],
-                    ['2005', 1170, 460, 250],
-                    ['2006', 660, 1120, 300],
-                    ['2007', 1030, 540, 350],
-                ]);
+                var data = GoogleCharts.api.visualization.arrayToDataTable(this.graphData);
 
                 // Set chart options
                 var options = {
-                    title: 'Company Performance',
+                    title: 'Account evolution',
                     curveType: 'function',
                     legend: { position: 'bottom' },
                 };
@@ -42,10 +38,43 @@ export default {
                 chart.draw(data, options);
             }, { packages: ['corechart'] });
         },
+        getCurrentInfo() {
+            const token = localStorage.getItem('user-token'); // get the token from local storage
+            axios.get('http://localhost:3000/cashflowlog', {
+                headers: {
+                    Authorization: `Bearer ${token}` // send the token in the Authorization header
+                }
+            })
+                .then(response => {
+                    this.cashflow = response.data;
+                    for (let log of this.cashflow) {
+                        if (log.type.toLowerCase() == 'income') {
+                            this.accountValues[log.currency.toLowerCase()] += log.value;
+                        } else if (log.type.toLowerCase() == 'expense') {
+                            this.accountValues[log.currency.toLowerCase()] -= log.value;
+                        }
+
+                        // add the current info to the graph data
+                        this.graphData.push([log.date, this.accountValues['ron'], this.accountValues['eur'], this.accountValues['usd']]);
+                    }
+                    this.drawChart();
+                })
+                .catch(error => {
+                    localStorage.removeItem('user-token');
+                    this.toastMessage = 'Invalid login: ' + error.response.data.message;
+                    this.showToast = true;
+
+                    setTimeout(() => {
+                        this.showToast = false;
+                    }, 5000);
+                    this.$router.push('/login');
+                    return;
+                });
+        },
     },
-    mounted() {
-        this.drawChart();
-    },
+    created() {
+        this.getCurrentInfo();
+    }
 }
 </script>
 
