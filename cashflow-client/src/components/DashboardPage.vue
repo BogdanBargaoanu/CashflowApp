181ebbf7e77cbfac50790e74faf07bf1bb8e4a12
BogdanBargaoanu <bn19bba@yahoo.com>
2024-05-31 10:18:10 +0300

    html css + variables for current balance

--- cashflow-client/src/components/DashboardPage.vue
+++ cashflow-client/src/components/DashboardPage.vue
@@ -1,21 +1,27 @@
 <template>
     <h1 class="text-center dashboard-heading">Dashboard</h1>
     <div class="container-fluid dashboard">
-        <div class="content-box cb1">current-info
+        <div class="content-box cb1">
             <img src="../assets/dashboard-img/current-info.png" alt="current-info" class="current-info-icon">
+            <span class="dashboard-text account">Current account</span>
+            <span class="current-balance" :class="{ 'text-danger': this.currentBalanceLei < 0, 'text-success': this.currentBalanceLei >= 0 }">{{ this.currentBalanceLei }} LEI</span>
+            <span class="current-balance" :class="{ 'text-danger': this.currentBalanceEuro < 0, 'text-success': this.currentBalanceEuro >= 0 }">{{ this.currentBalanceEuro }} EURO</span>
+            <span class="current-balance" :class="{ 'text-danger': this.currentBalanceUSD < 0, 'text-success': this.currentBalanceUSD >= 0 }">{{ this.currentBalanceUSD }} USD</span>
         </div>
         <div class="content-box cb2">
             <img src="../assets/dashboard-img/chart-icon.png" alt="chart-icon" class="chart-icon">
             <div id="linechart"></div>
         </div>
-        <div @click="cashflowLog()" class="content-box cb3">cashflow
+        <div @click="cashflowLog()" class="content-box cb3">
             <img src="../assets/dashboard-img/cashflow-log.png" alt="cashflow-log" class="cashflow-log-icon">
+            <span class="dashboard-text">Cashflow</span>
         </div>
-        <div class="content-box cb4">categories-graph
+        <div class="content-box cb4">
             <img src="../assets/dashboard-img/categories-graph.png" alt="categories-graph"
                 class="categories-graph-icon">
+            <span class="dashboard-text">Categories graphs</span>
         </div>
-        <div @click="entities()" class="content-box cb5">entities
+        <div @click="entities()" class="content-box cb5"><span class="dashboard-text">Entities</span>
         </div>
         <button @click="logout()" class="btn-logout"><i class="fa-solid fa-right-from-bracket"></i> Logout</button>
     </div>
@@ -25,6 +31,13 @@
 import { GoogleCharts } from 'google-charts';
 export default {
     name: 'DashboardPage',
+    data() {
+        return {
+            currentBalanceLei: 0,
+            currentBalanceEuro: 0,
+            currentBalanceUSD: 0
+        }
+    },
     methods: {
         logout() {
             localStorage.removeItem('user-token');
@@ -60,6 +73,9 @@ export default {
                 chart.draw(data, options);
             }, { packages: ['corechart'] });
         },
+        getCurrentBalance() {
+
+        }
     },
     mounted() {
         // Draw the chart when the component is mounted
